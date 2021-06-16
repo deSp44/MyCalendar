215a10ed3c4e658d54755fc6b3a36836ef015bc1
Michał Czaja <35144@studentwsei.pl>
2021-06-16 21:10:44 +0200

    Create an interactive menu and add a new class to manage various options

---
+++ CalendarService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalendarApp
+{
+    public class CalendarService
+    {
+        internal void ShowCalendar()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void ShowTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void AddNewEvent()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void AddNewTask()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void EditEventOrTask()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void DeleteEventOrTask()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyCalendarApp.MainMenuService;
+using System;
 
 namespace MyCalendarApp
 {
@@ -6,7 +7,71 @@ namespace MyCalendarApp
     {
         public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var actionService = new MenuActionService();
+            var calendarService = new CalendarService();
+            actionService = Initialize(actionService);
+
+            Console.WriteLine("Welcome to MyCalendar, the place where you manage your time!\n");
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Select operation:");
+                var mainMenu = actionService.GetMenuActionsByMenuName("Main");
+                foreach (var line in mainMenu)
+                    Console.WriteLine($"{line.Id}. {line.Name}");
+                Console.Write("> ");
+
+                var operation = Console.ReadKey();
+                switch (operation.KeyChar)
+                {
+                    case '1':
+                        Console.Clear();
+                        calendarService.ShowCalendar();
+                        break;
+                    case '2':
+                        Console.Clear();
+                        calendarService.ShowTasks();
+                        break;
+                    case '3':
+                        Console.Clear();
+                        calendarService.AddNewEvent();
+                        break;
+                    case '4':
+                        Console.Clear();
+                        calendarService.AddNewTask();
+                        break;
+                    case '5':
+                        Console.Clear();
+                        calendarService.EditEventOrTask();
+                        break;
+                    case '6':
+                        Console.Clear();
+                        calendarService.DeleteEventOrTask();
+                        break;
+                    case '7':
+                        Console.Clear();
+                        Console.Write("Goodbye!\n");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.Write("There is no such option. Choose a different key.");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
+
+        private static MenuActionService Initialize(MenuActionService actionService)
+        {
+            actionService.AddNewAction(1, "Show calendar", "Main");
+            actionService.AddNewAction(2, "Show tasks", "Main");
+            actionService.AddNewAction(3, "Add a new event", "Main");
+            actionService.AddNewAction(4, "Add a new task", "Main");
+            actionService.AddNewAction(5, "Edit event or task", "Main");
+            actionService.AddNewAction(6, "Delete event or task", "Main");
+            actionService.AddNewAction(7, "Exit", "Main");
+
+            return actionService;
         }
     }
 }
