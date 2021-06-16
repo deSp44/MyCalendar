be956141c73bcea683b4068adbf3fd860e656231
Michał Czaja <35144@studentwsei.pl>
2021-06-16 19:08:30 +0200

    Create enum for colors

---
+++ Helpers/CalendarColor.cs
@@ -0,0 +1,14 @@
+﻿namespace MyCalendarApp.Helpers
+{
+    public enum CalendarColor
+    {
+        Red = 1,
+        Yellow,
+        Green,
+        Blue,
+        Purple,
+        Brown,
+        Pink,
+        Orange,
+    }
+}
