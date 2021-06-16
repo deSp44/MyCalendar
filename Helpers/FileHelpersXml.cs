62b42bccd75ac92fa5dc6d4de746cded6468bb5d
Michał Czaja <35144@studentwsei.pl>
2021-06-16 20:48:50 +0200

    Add a helper class for managing and saving data in .xml files

---
+++ Helpers/FileHelpersXml.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace MyCalendarApp.Helpers
+{
+    public class FileHelpersXml<T> where T : new()
+    {
+        private readonly string _filePath;
+
+        public FileHelpersXml(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void SerializeToFile(T param)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+
+            using var streamWriter = new StreamWriter(_filePath);
+            serializer.Serialize(streamWriter, param);
+            streamWriter.Close();
+        }
+
+        public T DeserializeFromFile()
+        {
+            if (!File.Exists(_filePath))
+                return new T();
+
+            var serializer = new XmlSerializer(typeof(T));
+
+            using var streamReader = new StreamReader(_filePath);
+            var collectedData = (T)serializer.Deserialize(streamReader);
+            streamReader.Close();
+
+            return collectedData;
+        }
+    }
+}
